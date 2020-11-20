cf02edc8fe67447f7a7b8476b12230302e0bf138
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-11-20 14:07:05 +0100

    subclassing: just print vfuncs

--- src/codegen/subclass/mod.rs
+++ src/codegen/subclass/mod.rs
@@ -1,7 +1,27 @@
-use crate::{codegen::generate_single_version_file, env::Env, version::Version};
+use crate::{codegen::generate_single_version_file, env::Env, library::{MAIN_NAMESPACE, Type}, version::Version};
 use std::collections::BTreeMap;
 
 
 pub fn generate(env: &Env) {
-    env.library.show_non_bound_types(env);
+    for x in env.library.namespace(MAIN_NAMESPACE).types.iter() {
+        match x {
+            Some(Type::Class(c)) =>  {
+
+                c.virtual_methods.iter().for_each(|v| {
+                    println!("[ ] {} - {:#?}", c.name, v.name);
+                });
+                println!("##############");
+            },
+            Some(Type::Interface(i)) => {
+                i.virtual_methods.iter().for_each(|v| {
+                    println!("[ ] {} - {:#?}", i.name, v.name);
+                });
+                println!("##############");
+            },
+            _ => (),
+        }
+    }
 }
+
+
+
